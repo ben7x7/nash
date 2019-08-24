2f6e0623720b6296707afa40ca67c1638a47a349
benoit bessiere <benoit.bessiere@gmail.com>
2019-08-24 12:10:35 +0200

    add frontend setup + background video

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Nash
   class Application < Rails::Application
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
